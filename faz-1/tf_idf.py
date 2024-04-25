1343cf156fbf9123f4213da87f8aa49e0f788d62
Aleyna KAHRAMAN <72937626+aleykhrmn@users.noreply.github.com>
2024-04-25 21:21:14 +0300

    tf-idf kodu güncellendi.

--- faz-1/tf_idf.py
+++ faz-1/tf_idf.py
@@ -39,8 +39,8 @@ if os.path.exists(file_path):
 
     # Gaussian Naive Bayes modelini oluşturma ve eğitme
     GNB = GaussianNB()
-    GNB.fit(X_train.toarray(), y_train)  # GaussianNB, yoğun (dense) matris gerektirir
-
+    GNB.fit(X_train.toarray(), y_train) 
+    
     # Bernoulli Naive Bayes modelini oluşturma ve eğitme
     BNB = BernoulliNB()
     BNB.fit(X_train, y_train)
@@ -58,7 +58,7 @@ if os.path.exists(file_path):
     DT.fit(X_train, y_train)
 
     # Random Forest modelini oluşturma ve eğitme
-    RF = RandomForestClassifier()
+    RF = RandomForestClassifier(n_estimators=100, random_state=42)
     RF.fit(X_train, y_train)
     
     # Yapay Sinir Ağı modelini oluşturma ve eğitme
@@ -68,17 +68,6 @@ if os.path.exists(file_path):
     # SVM modelini oluşturma ve eğitme
     SVM = SVC()
     SVM.fit(X_train, y_train)
-
-    # Eğitim setlerinde model performanslarını değerlendirme
-    cnb_train_accuracy = CNB.score(X_train, y_train)
-    gnb_train_accuracy = GNB.score(X_train.toarray(), y_train)  
-    bnb_train_accuracy = BNB.score(X_train, y_train)
-    mnb_train_accuracy = MNB.score(X_train, y_train)
-    lr_train_accuracy = LR.score(X_train, y_train)
-    dt_train_accuracy = DT.score(X_train, y_train)
-    rf_train_accuracy = RF.score(X_train, y_train)
-    ann_train_accuracy = ANN.score(X_train, y_train)
-    svm_train_accuracy = SVM.score(X_train, y_train)
 
     # Test setlerinde model performanslarını değerlendirme
     cnb_test_accuracy = CNB.score(X_test, y_test)
@@ -92,63 +81,21 @@ if os.path.exists(file_path):
     svm_test_accuracy = SVM.score(X_test, y_test)
 
     # Sonuçları yazdırma
-    print("Complement Naive Bayes Eğitim Seti Accuracy:", cnb_train_accuracy)
-    print("Gaussian Naive Bayes Eğitim Seti Accuracy:", gnb_train_accuracy)
-    print("Bernoulli Naive Bayes Eğitim Seti Accuracy:", bnb_train_accuracy)
-    print("Multinomial Naive Bayes Eğitim Seti Accuracy:", mnb_train_accuracy)
-    print("Logistic Regression Eğitim Seti Accuracy:", lr_train_accuracy)
-    print("Decision Tree Eğitim Seti Accuracy:", dt_train_accuracy)
-    print("Random Forest Eğitim Seti Accuracy:", rf_train_accuracy)
-    print("Yapay Sinir Ağı Eğitim Seti Accuracy:", ann_train_accuracy)
-    print("\nComplement Naive Bayes Test Set Accuracy:", cnb_test_accuracy)
+    print("Bernoulli Naive Bayes Test Set Accuracy:", bnb_test_accuracy)    
+    print("Complement Naive Bayes Test Set Accuracy:", cnb_test_accuracy)
     print("Gaussian Naive Bayes Test Set Accuracy:", gnb_test_accuracy)
-    print("Bernoulli Naive Bayes Test Set Accuracy:", bnb_test_accuracy)
     print("Multinomial Naive Bayes Test Set Accuracy:", mnb_test_accuracy)
     print("Logistic Regression Test Set Accuracy:", lr_test_accuracy)
-    print("Decision Tree Test Set Accuracy:", dt_test_accuracy)
     print("Random Forest Test Set Accuracy:", rf_test_accuracy)
+    print("Decision Tree Test Set Accuracy:", dt_test_accuracy)
     print("Yapay Sinir Ağı Test Seti Accuracy:", ann_test_accuracy)
-    print("SVM Eğitim Seti Accuracy:", svm_train_accuracy)
     print("SVM Test Seti Accuracy:", svm_test_accuracy)
 
-    # Eğitim setlerinde sınıflandırma raporu
-    print("\nComplement Naive Bayes Eğitim Seti Sınıflandırma Raporu:")
-    cnb_train_pred = CNB.predict(X_train)
-    print(classification_report(y_train, cnb_train_pred, zero_division=1))
-
-    print("\nGaussian Naive Bayes Eğitim Seti Sınıflandırma Raporu:")
-    gnb_train_pred = GNB.predict(X_train.toarray())  
-    print(classification_report(y_train, gnb_train_pred, zero_division=1))
-
-    print("\nBernoulli Naive Bayes Eğitim Seti Sınıflandırma Raporu:")
-    bnb_train_pred = BNB.predict(X_train)
-    print(classification_report(y_train, bnb_train_pred, zero_division=1))
-
-    print("\nMultinomial Naive Bayes Eğitim Seti Sınıflandırma Raporu:")
-    mnb_train_pred = MNB.predict(X_train)
-    print(classification_report(y_train, mnb_train_pred, zero_division=1))
-
-    print("\nLogistic Regression Eğitim Seti Sınıflandırma Raporu:")
-    lr_train_pred = LR.predict(X_train)
-    print(classification_report(y_train, lr_train_pred, zero_division=1))
-
-    print("\nDecision Tree Eğitim Seti Sınıflandırma Raporu:")
-    dt_train_pred = DT.predict(X_train)
-    print(classification_report(y_train, dt_train_pred, zero_division=1))
-
-    print("\nRandom Forest Eğitim Seti Sınıflandırma Raporu:")
-    rf_train_pred = RF.predict(X_train)
-    print(classification_report(y_train, rf_train_pred, zero_division=1))
+    # Test setlerinde sınıflandırma raporu    
+    print("\nBernoulli Naive Bayes Test Seti Sınıflandırma Raporu:")
+    bnb_test_pred = BNB.predict(X_test)
+    print(classification_report(y_test, bnb_test_pred, zero_division=1))
     
-    print("\nYapay Sinir Ağı Eğitim Seti Sınıflandırma Raporu:")
-    ann_train_pred = ANN.predict(X_train)
-    print(classification_report(y_train, ann_train_pred, zero_division=1))
-
-    print("\nSVM Eğitim Seti Sınıflandırma Raporu:")
-    svm_train_pred = SVM.predict(X_train)
-    print(classification_report(y_train, svm_train_pred, zero_division=1))
-
-    # Test setlerinde sınıflandırma raporu
     print("\nComplement Naive Bayes Test Seti Sınıflandırma Raporu:")
     cnb_test_pred = CNB.predict(X_test)
     print(classification_report(y_test, cnb_test_pred, zero_division=1))
@@ -156,10 +103,6 @@ if os.path.exists(file_path):
     print("\nGaussian Naive Bayes Test Seti Sınıflandırma Raporu:")
     gnb_test_pred = GNB.predict(X_test.toarray()) 
     print(classification_report(y_test, gnb_test_pred, zero_division=1))
-
-    print("\nBernoulli Naive Bayes Test Seti Sınıflandırma Raporu:")
-    bnb_test_pred = BNB.predict(X_test)
-    print(classification_report(y_test, bnb_test_pred, zero_division=1))
 
     print("\nMultinomial Naive Bayes Test Seti Sınıflandırma Raporu:")
     mnb_test_pred = MNB.predict(X_test)
@@ -169,14 +112,14 @@ if os.path.exists(file_path):
     lr_test_pred = LR.predict(X_test)
     print(classification_report(y_test, lr_test_pred, zero_division=1))
 
-    print("\nDecision Tree Test Seti Sınıflandırma Raporu:")
-    dt_test_pred = DT.predict(X_test)
-    print(classification_report(y_test, dt_test_pred, zero_division=1))
-
     print("\nRandom Forest Test Seti Sınıflandırma Raporu:")
     rf_test_pred = RF.predict(X_test)
     print(classification_report(y_test, rf_test_pred, zero_division=1))
     
+    print("\nDecision Tree Test Seti Sınıflandırma Raporu:")
+    dt_test_pred = DT.predict(X_test)
+    print(classification_report(y_test, dt_test_pred, zero_division=1))
+
     print("\nYapay Sinir Ağı Test Seti Sınıflandırma Raporu:")
     ann_test_pred = ANN.predict(X_test)
     print(classification_report(y_test, ann_test_pred, zero_division=1))
@@ -184,13 +127,6 @@ if os.path.exists(file_path):
     print("\nSVM Test Seti Sınıflandırma Raporu:")
     svm_test_pred = SVM.predict(X_test)
     print(classification_report(y_test, svm_test_pred, zero_division=1))
-
-    # Tüm TF-IDF matrisini metin dosyasına kaydetme
-    with open('tfidf_matrix.txt', 'w') as file:
-        for row in tfidf_matrix.toarray():
-            file.write(' '.join([str(elem) for elem in row]) + '\n')
-
-    print("TF-IDF Matrisi başarıyla metin dosyasına kaydedildi.")
 
 else:
     print("Dosya bulunamadı veya açılamadı. Lütfen dosya yolunu kontrol edin.")
