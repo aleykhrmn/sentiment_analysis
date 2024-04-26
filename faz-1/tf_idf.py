b57c902cf3ec3f4c7a72961410bf5c04c2f37249
Aleyna KAHRAMAN <72937626+aleykhrmn@users.noreply.github.com>
2024-04-26 23:34:48 +0300

    tf-idf + makine öğrenimi kodu tamamlandı.

--- faz-1/tf_idf.py
+++ faz-1/tf_idf.py
@@ -1,15 +1,18 @@
 import os
 import pandas as pd
-import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import ComplementNB, GaussianNB, BernoulliNB, MultinomialNB
+from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+import xgboost as xgb
+import lightgbm as lgb
 
 # Birleşik veri dosyasının yolu
 file_path = 'data.xlsx'
@@ -30,20 +33,12 @@ if os.path.exists(file_path):
     # Duygu puanlarını hedef değişken olarak kullanma
     y = df['duygu']
 
+    # Etiketleri sayısal değerlere dönüştürme
+    label_encoder = LabelEncoder()
+    y_encoded = label_encoder.fit_transform(y)
+
     # Eğitim ve test setlerine ayırma
-    X_train, X_test, y_train, y_test = train_test_split(tfidf_matrix, y, test_size=0.2, random_state=42)
-
-    # Complement Naive Bayes modelini oluşturma ve eğitme
-    CNB = ComplementNB()
-    CNB.fit(X_train, y_train)
-
-    # Gaussian Naive Bayes modelini oluşturma ve eğitme
-    GNB = GaussianNB()
-    GNB.fit(X_train.toarray(), y_train) 
-    
-    # Bernoulli Naive Bayes modelini oluşturma ve eğitme
-    BNB = BernoulliNB()
-    BNB.fit(X_train, y_train)
+    X_train, X_test, y_train, y_test = train_test_split(tfidf_matrix, y_encoded, test_size=0.2, random_state=42)
 
     # Multinomial Naive Bayes modelini oluşturma ve eğitme
     MNB = MultinomialNB()
@@ -69,40 +64,41 @@ if os.path.exists(file_path):
     SVM = SVC()
     SVM.fit(X_train, y_train)
 
+    # KNN modelini oluşturma ve eğitme
+    KNN = KNeighborsClassifier()
+    KNN.fit(X_train, y_train)
+    
+    # XGBoost modelini oluşturma ve eğitme
+    XGB = xgb.XGBClassifier()
+    XGB.fit(X_train, y_train)
+    
+    # LightGBM modelini oluşturma ve eğitme
+    LGB = lgb.LGBMClassifier(verbose=0)
+    LGB.fit(X_train, y_train)
+
     # Test setlerinde model performanslarını değerlendirme
-    cnb_test_accuracy = CNB.score(X_test, y_test)
-    gnb_test_accuracy = GNB.score(X_test.toarray(), y_test)  
-    bnb_test_accuracy = BNB.score(X_test, y_test)
     mnb_test_accuracy = MNB.score(X_test, y_test)
     lr_test_accuracy = LR.score(X_test, y_test)
     dt_test_accuracy = DT.score(X_test, y_test)
     rf_test_accuracy = RF.score(X_test, y_test)
     ann_test_accuracy = ANN.score(X_test, y_test)
     svm_test_accuracy = SVM.score(X_test, y_test)
+    knn_test_accuracy = KNN.score(X_test, y_test)
+    xgb_test_accuracy = XGB.score(X_test, y_test)
+    lgb_test_accuracy = LGB.score(X_test, y_test)
 
     # Sonuçları yazdırma
-    print("Bernoulli Naive Bayes Test Set Accuracy:", bnb_test_accuracy)    
-    print("Complement Naive Bayes Test Set Accuracy:", cnb_test_accuracy)
-    print("Gaussian Naive Bayes Test Set Accuracy:", gnb_test_accuracy)
     print("Multinomial Naive Bayes Test Set Accuracy:", mnb_test_accuracy)
     print("Logistic Regression Test Set Accuracy:", lr_test_accuracy)
     print("Random Forest Test Set Accuracy:", rf_test_accuracy)
     print("Decision Tree Test Set Accuracy:", dt_test_accuracy)
     print("Yapay Sinir Ağı Test Seti Accuracy:", ann_test_accuracy)
     print("SVM Test Seti Accuracy:", svm_test_accuracy)
+    print("KNN Test Seti Accuracy:", knn_test_accuracy)
+    print("XGBoost Test Seti Accuracy:", xgb_test_accuracy)
+    print("LightGBM Test Seti Accuracy:", lgb_test_accuracy)
 
     # Test setlerinde sınıflandırma raporu    
-    print("\nBernoulli Naive Bayes Test Seti Sınıflandırma Raporu:")
-    bnb_test_pred = BNB.predict(X_test)
-    print(classification_report(y_test, bnb_test_pred, zero_division=1))
-    
-    print("\nComplement Naive Bayes Test Seti Sınıflandırma Raporu:")
-    cnb_test_pred = CNB.predict(X_test)
-    print(classification_report(y_test, cnb_test_pred, zero_division=1))
-
-    print("\nGaussian Naive Bayes Test Seti Sınıflandırma Raporu:")
-    gnb_test_pred = GNB.predict(X_test.toarray()) 
-    print(classification_report(y_test, gnb_test_pred, zero_division=1))
 
     print("\nMultinomial Naive Bayes Test Seti Sınıflandırma Raporu:")
     mnb_test_pred = MNB.predict(X_test)
@@ -127,6 +123,18 @@ if os.path.exists(file_path):
     print("\nSVM Test Seti Sınıflandırma Raporu:")
     svm_test_pred = SVM.predict(X_test)
     print(classification_report(y_test, svm_test_pred, zero_division=1))
+
+    print("\nKNN Test Seti Sınıflandırma Raporu:")
+    knn_test_pred = KNN.predict(X_test)
+    print(classification_report(y_test, knn_test_pred, zero_division=1))
+    
+    print("\nXGBoost Test Seti Sınıflandırma Raporu:")
+    xgb_test_pred = XGB.predict(X_test)
+    print(classification_report(y_test, xgb_test_pred, zero_division=1))
+    
+    print("\nLightGBM Test Seti Sınıflandırma Raporu:")
+    lgb_test_pred = LGB.predict(X_test)
+    print(classification_report(y_test, lgb_test_pred, zero_division=1))
 
 else:
     print("Dosya bulunamadı veya açılamadı. Lütfen dosya yolunu kontrol edin.")
