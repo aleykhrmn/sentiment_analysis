f2c8460971986b7101c5f5a9193c22fe62fdd165
Aleyna KAHRAMAN <72937626+aleykhrmn@users.noreply.github.com>
2024-05-13 18:38:43 +0300

    Tf-idf kodu tamamlandı.

--- faz-1/tf_idf.py
+++ faz-1/tf_idf.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -40,12 +40,12 @@ if os.path.exists(file_path):
     # Eğitim ve test setlerine ayırma
     X_train, X_test, y_train, y_test = train_test_split(tfidf_matrix, y_encoded, test_size=0.2, random_state=42)
 
-    # Multinomial Naive Bayes modelini oluşturma ve eğitme
-    MNB = MultinomialNB()
-    MNB.fit(X_train, y_train)
+    # Bernoulli Naive Bayes modelini oluşturma ve eğitme
+    BNB = BernoulliNB()
+    BNB.fit(X_train, y_train)
 
     # Logistic Regression modelini oluşturma ve eğitme
-    LR = LogisticRegression(max_iter=1000)
+    LR = LogisticRegression(max_iter=20000)
     LR.fit(X_train, y_train)
 
     # Decision Tree modelini oluşturma ve eğitme
@@ -53,11 +53,11 @@ if os.path.exists(file_path):
     DT.fit(X_train, y_train)
 
     # Random Forest modelini oluşturma ve eğitme
-    RF = RandomForestClassifier(n_estimators=100, random_state=42)
+    RF = RandomForestClassifier()
     RF.fit(X_train, y_train)
     
     # Yapay Sinir Ağı modelini oluşturma ve eğitme
-    ANN = MLPClassifier(hidden_layer_sizes=(100,), max_iter=1000)
+    ANN = MLPClassifier(hidden_layer_sizes=(100,), max_iter=20000)
     ANN.fit(X_train, y_train)
     
     # SVM modelini oluşturma ve eğitme
@@ -77,7 +77,7 @@ if os.path.exists(file_path):
     LGB.fit(X_train, y_train)
 
     # Test setlerinde model performanslarını değerlendirme
-    mnb_test_accuracy = MNB.score(X_test, y_test)
+    bnb_test_accuracy = BNB.score(X_test, y_test)
     lr_test_accuracy = LR.score(X_test, y_test)
     dt_test_accuracy = DT.score(X_test, y_test)
     rf_test_accuracy = RF.score(X_test, y_test)
@@ -88,21 +88,21 @@ if os.path.exists(file_path):
     lgb_test_accuracy = LGB.score(X_test, y_test)
 
     # Sonuçları yazdırma
-    print("Multinomial Naive Bayes Test Set Accuracy:", mnb_test_accuracy)
-    print("Logistic Regression Test Set Accuracy:", lr_test_accuracy)
-    print("Random Forest Test Set Accuracy:", rf_test_accuracy)
-    print("Decision Tree Test Set Accuracy:", dt_test_accuracy)
-    print("Yapay Sinir Ağı Test Seti Accuracy:", ann_test_accuracy)
-    print("SVM Test Seti Accuracy:", svm_test_accuracy)
-    print("KNN Test Seti Accuracy:", knn_test_accuracy)
-    print("XGBoost Test Seti Accuracy:", xgb_test_accuracy)
-    print("LightGBM Test Seti Accuracy:", lgb_test_accuracy)
+    print("Bernoulli Naive Bayes Test Seti Doğruluğu:", bnb_test_accuracy)
+    print("Logistic Regression Test Seti Doğruluğu:", lr_test_accuracy)
+    print("Random Forest Test Seti Doğruluğu:", rf_test_accuracy)
+    print("Decision Tree Test Seti Doğruluğu:", dt_test_accuracy)
+    print("SVM Test Seti Doğruluğu:", svm_test_accuracy)
+    print("Yapay Sinir Ağı Test Seti Doğruluğu:", ann_test_accuracy)
+    print("KNN Test Seti Doğruluğu:", knn_test_accuracy)
+    print("XGBoost Test Seti Doğruluğu:", xgb_test_accuracy)
+    print("LightGBM Test Seti Doğruluğu:", lgb_test_accuracy)
 
     # Test setlerinde sınıflandırma raporu    
 
-    print("\nMultinomial Naive Bayes Test Seti Sınıflandırma Raporu:")
-    mnb_test_pred = MNB.predict(X_test)
-    print(classification_report(y_test, mnb_test_pred, zero_division=1))
+    print("\nBernoulli Naive Bayes Test Seti Sınıflandırma Raporu:")
+    bnb_test_pred = BNB.predict(X_test)
+    print(classification_report(y_test, bnb_test_pred, zero_division=1))
 
     print("\nLogistic Regression Test Seti Sınıflandırma Raporu:")
     lr_test_pred = LR.predict(X_test)
@@ -115,14 +115,14 @@ if os.path.exists(file_path):
     print("\nDecision Tree Test Seti Sınıflandırma Raporu:")
     dt_test_pred = DT.predict(X_test)
     print(classification_report(y_test, dt_test_pred, zero_division=1))
+    
+    print("\nSVM Test Seti Sınıflandırma Raporu:")
+    svm_test_pred = SVM.predict(X_test)
+    print(classification_report(y_test, svm_test_pred, zero_division=1))
 
     print("\nYapay Sinir Ağı Test Seti Sınıflandırma Raporu:")
     ann_test_pred = ANN.predict(X_test)
     print(classification_report(y_test, ann_test_pred, zero_division=1))
-
-    print("\nSVM Test Seti Sınıflandırma Raporu:")
-    svm_test_pred = SVM.predict(X_test)
-    print(classification_report(y_test, svm_test_pred, zero_division=1))
 
     print("\nKNN Test Seti Sınıflandırma Raporu:")
     knn_test_pred = KNN.predict(X_test)
